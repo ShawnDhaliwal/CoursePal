4019ea70d92ca5ed147a3106f134882479f4129f
Shawn Dhaliwal <shawndhaliwal_28@Hotmail.com>
2016-05-23 17:33:45 -0700

    Syntax fix

--- session.php
+++ session.php
@@ -2,11 +2,11 @@
 
     session_start();
 
-     if(isset($_SESSION['User_log_in']){
+     if(isset($_SESSION['User_log_in'])){
         return true;
      }else {
         return false;
      }
-     }
+     
 
  ?>
